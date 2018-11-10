da4c401fe9452e524565dba93abd96b39260ef89
Allen Lozano <allen.lozano99@gmail.com>
2018-11-10 15:42:30 -0800

    Fixed bug and added logout

---
+++ twitter_alamofire_demo/TimelineViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TimelineViewController.swift
+//  twitter_alamofire_demo
+//
+//  Created by Aristotle on 2018-08-11.
+//  Copyright © 2018 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logout(_ sender: Any) {
+        self.performSegue(withIdentifier: "logoutSegue", sender: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
